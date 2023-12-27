a43b24a799ff0aceca5940fadd9947a437599045
Geetam <geetambinani6@gmail.com>
2023-12-27 14:17:10 +0530

    add dist

---
+++ dist/assets/Contact-29239eed.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-66c83e19.js";import{a as t}from"./Button-151706a8.js";function o(){return a.jsx("div",{className:"contact__container",children:a.jsx("a",{className:"mail__btn",href:"mailto:geetambinani6@gmail.com",children:a.jsx(t,{children:"Contact: mymailforgeetambinani6@gmail.com"})})})}export{o as default};
---
+++ dist/assets/Loader-795ab423.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-66c83e19.js";function o(){return s.jsx("div",{className:"loading",children:s.jsx("span",{className:"loader"})})}export{o as L};
